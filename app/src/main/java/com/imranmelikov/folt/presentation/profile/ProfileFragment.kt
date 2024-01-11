1b67ae43f2922a9191ca4ae811f6bd3666be7645
Imran Melikov <imran.melikov70@gmail.com>
2024-01-11 10:56:08 +0400

    fragmentAccount design ended

--- app/src/main/java/com/imranmelikov/folt/presentation/profile/ProfileFragment.kt
+++ app/src/main/java/com/imranmelikov/folt/presentation/profile/ProfileFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.imranmelikov.folt.R
 import com.imranmelikov.folt.databinding.FragmentProfileBinding
 
@@ -15,6 +16,9 @@ class ProfileFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding=FragmentProfileBinding.inflate(inflater,container,false)
+        binding.accountLinear.setOnClickListener {
+            findNavController().navigate(R.id.action_profileFragment_to_accountFragment)
+        }
         return binding.root
     }
 }
