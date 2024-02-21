e5b116962e37b84f536d85898bda14f0f495efb6
Imran Melikov <imran.melikov70@gmail.com>
2024-02-22 02:22:41 +0400

    address_rv,order_histrory_rv xmls added

--- app/src/main/java/com/imranmelikov/folt/domain/model/Order.kt
+++ app/src/main/java/com/imranmelikov/folt/domain/model/Order.kt
@@ -2,4 +2,4 @@ package com.imranmelikov.folt.domain.model
 
 data class Order(val id:String,val email:String,val userName:String,val userId:String,val tel:String,
     val deliveryTime: String,val itemSubtotal:Number,val contactDelivery:String,val totalPrice:Number,
-    val venueDetailsList: List<VenueDetails>,val paymentType:String,val address: Address)
+    val venueDetailsList: List<VenueDetails>,val paymentType:String,val address: Address,val history:String)
